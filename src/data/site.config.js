d57221065b57d9b25fe8e00de760f210181abce5
Genaro Hernandez <genarohp7@gmail.com>
2026-07-09 19:53:59 -0600

    Update Jorge Arias links

--- src/data/site.config.js
+++ src/data/site.config.js
@@ -5,5 +5,5 @@ export const siteConfig = {
   contactHref: '#consulta',
   contactEmail: 'contacto@ariasabogados.mx',
   mailchimpAction: 'https://ariasabogados.us13.list-manage.com/subscribe/post?u=d4dd807b38b4e68176971fb76&id=8768cdb372&f_id=00c803e9f0',
-  externalJorgeAriasUrl: 'https://jossergioj215.sg-host.com',
+  externalJorgeAriasUrl: 'https://jorgearias.mx/',
 };
